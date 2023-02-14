ca45b7d27a521101939c8dc59dfa3787b2a588a7
xujhuan <125333264+xujhuan@users.noreply.github.com>
2023-02-14 12:11:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,50 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx4838ba65a52b79c2',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '36f93213272aa67dec44aa53098e31e2',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '南京',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'jxd31415926535',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJXn56Xw6w893zkFeXYGtJ1IteFY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'BzA-FZpTz_WYMm0FNyeVqkK2YZI3MSImaJTj07nUkFg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-26',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: 'jxd31415926535', year: '1995', date: '03-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: 'd--2098', year: '2001', date: '09-20',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '02-10',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+     
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'BzA-FZpTz_WYMm0FNyeVqkK2YZI3MSImaJTj07nUkFg',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'd--2098',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJXn56ZYeAbBzuIESsD3kCu-MXrI',
     }
   ],
 
